74619d47858983b52cd9540322490513747bcc58
dhiru0904 <dhirajdevre4@gmail.com>
2022-10-25 10:02:41 +0530

    Added:uc2 Welcome to Daily Employee Wage

---
+++ src/employeewage/DailyEmployeeWage.java
@@ -0,0 +1,35 @@
+package employeewage;
+
+public class DailyEmployeeWage {
+    public  DailyEmployeeWage() {
+        System.out.println("Welcome to Employee Wage Program...**");
+    }
+
+    public static void main(String[] args) {
+
+        //Initializing the constructor by creating the object of the class.
+        DailyEmployeeWage employee = new  DailyEmployeeWage();
+        System.out.println();
+
+        //Declaring the variables.
+        double isPresent = 1;
+        double randomCheck = (double) Math.floor((Math.random() * 10) % 2);
+        int fullDayHour = 8;
+        int wagePerHour = 20;
+        int salary = 0;
+
+        //Checking if the Employee is present, then calculating his/her daily wage.
+        if(randomCheck == isPresent)
+        {
+            System.out.println("Employee is present");
+            salary = fullDayHour * wagePerHour;
+            System.out.println("Salary is: $"+salary);
+        }
+        else
+        {
+            System.out.println("Employee is absent");
+            System.out.println("Salary is: $"+salary);
+        }
+
+    }
+}
